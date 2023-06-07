503ed52d5de5827016023b524a81d080afd1043e
Cat1cry <1208343948@qq.com>
2023-06-08 07:51:49 +0800

    [Model] 新增：添加User实体类

--- Server/Model/MainDbContext.cs
+++ Server/Model/MainDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using Model.Model.Player;
 namespace Model;
 
 public class MainDbContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
+        // 这里填写数据库连接字符串
+        optionsBuilder.UseSqlServer("");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
