d4374da44238a02cf8dda06500eca38b29c41d93
Cat1cry <1208343948@qq.com>
2023-06-08 21:51:28 +0800

    [MainResource] 新增: 登录校验

--- Server/MainResource/Event/Connect.cs
+++ Server/MainResource/Event/Connect.cs
@@ -2,6 +2,7 @@ using System.Numerics;
 using AltV.Net;
 using AltV.Net.Elements.Entities;
 using MainResource.Log;
+using Repository.Repository;
 
 namespace MainResource.Event;
 
@@ -19,12 +20,15 @@ public class Connect : IScript
     public void TryLogin(IPlayer player, string username, string password)
     {
         if (player == null) return;
+        if (UserRepository.instance.Login(player,username,password))
+        {
+            player.Spawn(new Vector3((float)-1291.71, (float)83.43, (float)54.89)); // 生成 player
+            player.Model = 0xB8D69E3;
         
-        player.Spawn(new Vector3((float)-1291.71, (float)83.43, (float)54.89)); // 生成 player
-        player.Model = 0xB8D69E3;
+            player.Emit("auth:client:close");
         
-        player.Emit("auth:client:close");
+            player.Emit("client:Console", "登录成功 - 已生成");
+        }
         
-        player.Emit("client:Console", "登录成功 - 已生成");
     }
 }
