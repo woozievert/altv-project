b7cd4c2969945d20e9aee4bfca27cb42762f6de8
woozie <hasierbieke@gmail.com>
2023-06-08 19:08:26 +0800

    修改:客户端日志处理服务端方法

--- Client/src/log/handler.ts
+++ Client/src/log/handler.ts
@@ -1,7 +1,8 @@
 import * as alt from "alt-client"
+import * as logger from "./logger";
 
 alt.onServer('client:Console', handleLogConsole);
 
 function handleLogConsole(msg: string) {
-    alt.log('[服务端日志] ' + msg);
+    logger.info('[服务端日志] ' + msg);
 }
