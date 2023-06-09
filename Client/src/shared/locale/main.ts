5f7d4c6bfef3d8cc4af8dd96de609f9e4e8362b9
woozie <hasierbieke@gmail.com>
2023-06-09 21:08:04 +0800

    修改:客户端语言包常量对象更改为TS接口

--- Client/src/shared/locale/main.ts
+++ Client/src/shared/locale/main.ts
@@ -1,7 +1,11 @@
 import hansLocale from './languages/zh-hans'; // 简体中文语言包
 import hantLocale from './languages/zh-hant'; // 繁体中文语言包
 
-const langPack = {
+interface langPack {
+    [key: string]: any;
+}
+
+const langPack: langPack = {
     "zh-CN": hansLocale,
     "zh-TW": hantLocale,
 };
