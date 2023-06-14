f32869302c090d4fd907c6bae2b0a1f24a2902b3
woozie <hasierbieke@gmail.com>
2023-06-14 14:07:15 +0800

    新增:auth页面加载条和语言导入

--- Client/webview/auth/js/auth.js
+++ Client/webview/auth/js/auth.js
@@ -1,90 +1,95 @@
-let langPack = undefined;
 let who_in_window = 'alt';
 
-document.addEventListener('DOMContentLoaded', (event) => {
+const localesData = loadLocales().catch(error => {
+    console.error('加载 json 失败:', error);
+});
+
+document.addEventListener('DOMContentLoaded', async (event) => {
     if (html_dev) who_in_window = 'html_dev';
     if (`${who_in_window}` in window) {
-        console.log(`${who_in_window}加载完成`);
+        const locales = await localesData;
+        initLocales().catch(error => {
+            console.error('初始化语言失败:', error);
+        });
 
-        console.log(locale);
+        console.log(`${who_in_window}加载完成`);
 
         loginNotify('');
         regNotify('');
 
-        loginInputType.addEventListener("click",function(){
-            if(loginPassword.type === "password") loginPassword.type = "text";
+        loginInputType.addEventListener("click", function () {
+            if (loginPassword.type === "password") loginPassword.type = "text";
             else loginPassword.type = "password";
         });
 
-        regInputType.addEventListener("click",function(){
-            if(regPassword.type === "password") regPassword.type = "text";
+        regInputType.addEventListener("click", function () {
+            if (regPassword.type === "password") regPassword.type = "text";
             else regPassword.type = "password";
         });
 
-        verInputType.addEventListener("click",function(){
-            if(verPassword.type === "password") verPassword.type = "text";
+        verInputType.addEventListener("click", function () {
+            if (verPassword.type === "password") verPassword.type = "text";
             else verPassword.type = "password";
         });
 
-        toggleReg.addEventListener("click", function() {
+        toggleReg.addEventListener("click", function () {
             if (current_page === 'login') {
                 current_page = 'register';
 
-                loginForm.style.display = "none";
-                registerForm.style.display = "block";
+                hideElement(loginForm);
+                displayElement(registerForm);
                 formTitle.textContent = "注册窗口";
                 toggleLogin.textContent = "返回登录";
             }
         });
 
-        toggleLogin.addEventListener("click", function() {
+        toggleLogin.addEventListener("click", function () {
             if (current_page === 'register') {
                 current_page = 'login';
 
-                loginForm.style.display = "block";
-                registerForm.style.display = "none";
+                displayElement(loginForm);
+                hideElement(registerForm);
                 formTitle.textContent = "登录窗口";
                 toggleReg.textContent = "创建一个账号";
             }
         });
 
-        loginButton.addEventListener("click", function() {
+        loginButton.addEventListener("click", function () {
+            console.log('click')
             if (loginUser.value.toString() != null && loginPassword.value.toString() != null) {
                 alt.emit('auth:client:tryLogin', loginUser.value.toString(), loginPassword.value.toString());
             }
         });
 
+        await hideElement(regEmailType);
         regEmail.addEventListener("keyup", function () {
-            if (regEmail.value.toString() === '') regEmailType.style.display = 'none';
-            else regEmailType.style.display = 'block';
+            if (regEmail.value.toString() === '') hideElement(regEmailType);
+            else displayElement(regEmailType);
 
             if (!isValidEmail(regEmail.value.toString())) {
-                regEmailType.style.display = 'block';
+                displayElement(regEmailType);
                 regNotify('邮箱格式有误！');
-            }
-            else {
-                console.log('合格')
+            } else {
                 regNotify('');
-                regEmailType.style.display = 'none';
+                hideElement(regEmailType);
             }
         });
 
-        regUser.addEventListener("keyup", function() {
+        regUser.addEventListener("keyup", function () {
             if (!isValidUsername(regUser.value.toString())) regNotify('用户名有误(4-16字符,可含字母，数字，下划线，减号)');
             else regNotify('');
         });
 
-        verPassword.addEventListener("keyup", function() {
+        verPassword.addEventListener("keyup", function () {
             if (!isValidPassword(verPassword.value.toString())) regNotify('密码有误(6-20字符,包含至少1个大、小写字母、数字)');
             else {
                 if (verPassword.value.toString() !== regPassword.value.toString()) {
                     regNotify('两次密码不一致！');
-                }
-                else regNotify('');
+                } else regNotify('');
             }
         });
 
-        registerButton.addEventListener("click", function() {
+        registerButton.addEventListener("click", function () {
             if (regUser.value.toString() != null && regPassword.value.toString() != null && regEmail.value.toString() != null) {
                 if (!isValidUsername(regUser.value.toString())) return regNotify('用户名有误(4-16字符,可含字母，数字，下划线，减号)');
                 else if (!isValidPassword(regPassword.value.toString())) return regNotify('密码有误(6-20字符,包含至少1个大、小写字母、数字)');
@@ -98,33 +103,42 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 if (loginUser.value.toString() != null && loginPassword.value.toString() != null) {
                     alt.emit('auth:client:saveLocalAuth', loginUser.value.toString(), loginPassword.value.toString());
                 }
-            }
-            else alt.emit('auth:client:deleteLocalAuth');
+            } else alt.emit('auth:client:deleteLocalAuth');
         });
 
-        newsImg.addEventListener('click', function() {
-            loginForm.style.display = 'none';
-            registerForm.style.display = 'none';
-            newsForm.style.display = 'none';
+        console.log(newsImg)
+        newsImg.addEventListener('click', function () {
+            console.log('click')
+
+            hideElement(loginForm);
+            hideElement(registerForm);
+            hideElement(newsForm);
             bigImg.setAttribute("src", newsImg.src);
-            bigImgDiv.style.display = 'block';
+            displayElement(bigImgDiv);
         });
 
-        bigImg.addEventListener('click', function() {
-            if (current_page === 'login') loginForm.style.display = 'block';
-            else registerForm.style.display = 'block';
+        bigImg.addEventListener('click', function () {
+            if (current_page === 'login') displayElement(loginForm);
+            else displayElement(registerForm);
 
-            newsForm.style.display = 'block';
+            displayElement(newsForm);
             bigImg.setAttribute("src", '');
-            bigImgDiv.style.display = 'none';
+            hideElement(bigImgDiv);
         });
 
-        // alt.on('auth:webview:importLangPack', importLangPack);
-        // function importLangPack(data) {
-        //     console.log('已加载')
-        //     langPack = data;
-        //     console.log(langPack);
-        // }
+        async function initLocales() {
+            formTitle.textContent = locales['login.text.title'];
+            placeUser.textContent = locales['login.place.user'];
+            placePass.textContent = locales['login.place.pass'];
+            savePass.textContent = locales['login.input.save_pass'];
+            loginButton.value = locales['login.button.submit'];
+            otherLogin.textContent = locales['login.text.other'];
+            noAccount.textContent = locales['login.text.no_account'];
+            toggleReg.textContent = locales['login.link.register'];
+            loginForgetPass.textContent = locales['login.link.forget_pass'];
+
+            setTimeout(finishBuild, _getRandomTick());
+        }
 
         alt.on('auth:webview:getLocalAuth', _getLocalAuth);
         alt.on('auth:webview:wrongAuth', loginNotify);
@@ -132,6 +146,38 @@ document.addEventListener('DOMContentLoaded', (event) => {
         alt.on('auth:webview:finishReg', regNotify);
     }
 });
+
+function _getRandomTick() {
+    const min = 3;
+    const max = 7.5;
+    const decimalOptions = [0, 0.5];
+
+    const randomNumber = Math.random() * (max - min) + min;
+    const decimalPart = decimalOptions[Math.floor(Math.random() * decimalOptions.length)];
+
+    return (Math.floor(randomNumber) + decimalPart) * 1000;
+}
+
+async function displayElement(element) {
+    element.classList.remove('hidden');
+    element.visibility = '';
+}
+
+async function hideElement(element) {
+    element.classList.add('hidden');
+}
+
+async function finishBuild() {
+    await hideElement(loader);
+    await displayElement(copyright);
+    await displayElement(loginForm);
+    await displayElement(newsDiv);
+}
+
+async function loadLocales() {
+    const response = await fetch('../../../locales/zh_hans.json');
+    return await response.json();
+}
 
 function isValidEmail(email) {
     // 邮箱正则表达式
--- Client/webview/auth/js/index.js
+++ Client/webview/auth/js/index.js
@@ -10,19 +10,27 @@ const toggleLogin = document.querySelector("#toggle-login"); // 切换至登录
 const loginForm = document.querySelector("#login-form"); // 登录窗口
 const registerForm = document.querySelector("#reg-form"); // 注册窗口
 const formTitle = document.querySelector("#form-title"); // 窗口标题
+const placeUser = document.querySelector("#form-username"); // 用户名 placeholder
+const placePass = document.querySelector("#form-password"); // 密码 placeholder
+const savePass = document.querySelector("#save-password"); // 记住密码
+const otherLogin = document.querySelector("#other-login"); // 其他方式登录
+const noAccount = document.querySelector("#no-account"); // 还没有账号？
+const loginForgetPass = document.querySelector("#login-forget-pass"); // 还没有账号？
 const regUser = document.querySelector('#regUsernameInput');
 const regPassword = document.querySelector('#regPasswordInput');
 const verPassword = document.querySelector('#verPasswordInput');
 const regEmail = document.querySelector('#regEmailInput');
 const registerButton = document.querySelector(".reg-button");
 const checkBox = document.querySelector(".save-info");
+const newsDiv = document.querySelector(".news-div");
 const newsForm = document.querySelector(".news-form");
 const newsImg = document.querySelector(".news-img");
 const bigImgDiv = document.querySelector(".big-image-div");
 const bigImg = document.querySelector(".big-image");
 
-const locale = document.querySelector("#localeJson").innerHTML;
+const loader = document.querySelector('.loader');
+const copyright = document.querySelector('.copyright');
 
-var html_dev = false;
+var html_dev = true;
 var current_page = 'login';
 
