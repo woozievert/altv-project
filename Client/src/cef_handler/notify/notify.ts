a4dfaa2d68e264ff227d0e30c9afe54abda30ba5
woozie <hasierbieke@gmail.com>
2023-06-26 20:07:51 +0800

    修改:客户端通知异步

--- Client/src/cef_handler/notify/notify.ts
+++ Client/src/cef_handler/notify/notify.ts
@@ -26,15 +26,15 @@ export async function error(text: string) {
 }
 
 // 通知系统客户端调用事件，准确来说web用: alt.emit('notify:server:send', 'info', '服务端来的消息，注意查收。');
-alt.on('notify:client:send', (type: string, text: string) => {
-    if (type == 'info') info(text);
-    else if (type == 'green') green(text);
-    else if (type == 'error') error(text);
+alt.on('notify:client:send', async (type: string, text: string) => {
+    if (type == 'info') await info(text);
+    else if (type == 'green') await green(text);
+    else if (type == 'error') await error(text);
 });
 
 // 通知系统服务端调用事件: player.Emit('notify:server:send', 'info', '服务端来的消息，注意查收。');
-alt.onServer('notify:server:send', (type: string, text: string) => {
-    if (type == 'info') info(text);
-    else if (type == 'green') green(text);
-    else if (type == 'error') error(text);
+alt.onServer('notify:server:send', async (type: string, text: string) => {
+    if (type == 'info') await info(text);
+    else if (type == 'green') await green(text);
+    else if (type == 'error') await error(text);
 });
